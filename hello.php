9174aa54aedc732fc21febf8bd74462d72ae5d2a
Mio Miguel Galang <mgalang@zendesk.com>
2015-05-27 11:02:17 +0800

    Bumping v1.2.4

--- hello.php
+++ hello.php
@@ -4,7 +4,7 @@ Plugin Name: Awesome MGalang
 Plugin URI: http://wordpress.org/extend/plugins/awesome-mgalang/
 Description: Everything is awesome
 Author: Mio Galang
-Version: 1.2.3
+Version: 1.2.4
 */
 
 function get_expression() {
