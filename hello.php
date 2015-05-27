6892495c35bfd202b630324a88c2dfcb1a39bae9
Mio Miguel Galang <mgalang@zendesk.com>
2015-05-27 15:44:43 +0800

    Bump v1.3.1

--- hello.php
+++ hello.php
@@ -4,7 +4,7 @@
  * Plugin URI: http://wordpress.org/extend/plugins/awesome-mgalang/
  * Description: Everything is awesome
  * Author: Mio Galang
- * Version: 1.3
+ * Version: 1.3.1
  */
 
 function get_expression() {
